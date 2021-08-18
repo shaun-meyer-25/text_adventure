261ed0cbcfc1a1afd9f0df078f3f99374464f34d
Shaun Meyer <meyersak@uni.edu>
2021-08-17 23:51:03 -0500

    add animated bear

--- Assets/Scripts/Effects/BearHitboxes.cs
+++ Assets/Scripts/Effects/BearHitboxes.cs
@@ -6,12 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class BearHitboxes : MonoBehaviour
 {
-    public Collider2D headHitbox;
     public Collider2D fatalHitbox;
-    public Collider2D nonfatalHitbox;
-    public Animator animator;
     private Camera _camera;
     public IController Controller;
+    public bool isDead = false;
 
     private void Start()
     {
@@ -20,64 +18,41 @@ public class BearHitboxes : MonoBehaviour
 
     private void Update()
     {
+        
         if (Input.GetMouseButtonDown(0))
         {
-            bool isFatal = false;
-            bool isHead = false;
-            bool inCollider = false;
-            Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
-            if (headHitbox.OverlapPoint(mousePosition))
+            if (!isDead)
             {
-                isHead = true;
-                inCollider = true;
-            }
+                bool isFatal = false;
+                bool isHead = false;
+                bool inCollider = false;
+                Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
 
-            if (fatalHitbox.OverlapPoint(mousePosition))
-            {
-                inCollider = true;
-                isFatal = true;
-            }
-
-            if (nonfatalHitbox.OverlapPoint(mousePosition))
-            {
-                inCollider = true;
-                
-            }
-
-            if (!isFatal && inCollider)
-            {
-                if (isHead)
+                if (fatalHitbox.OverlapPoint(mousePosition))
                 {
-                    Controller.LogStringWithReturn("the bones in its head are too strong. you will only injure it this way.");
+                    isFatal = true;
+                    isDead = true;
+                }
+
+                if (isFatal)
+                {
+                    StartCoroutine(ChangeSceneAfter5());
                 }
                 else
                 {
-                    Controller.LogStringWithReturn("you will only injure it this way. you must strike its heart.");
+                    Controller.LogStringWithReturn("reality only allows one chance. that will not be a fatal strike.");
                 }
-                animator.enabled = true;
-                animator.GetComponentInParent<SpriteRenderer>().enabled = true;
-                animator.SetTrigger("BearChomp");
-                StartCoroutine(DisableAfter2());
-            } else if (isFatal)
-            {
-                StartCoroutine(ChangeSceneAfter5());
             }
-
         }
     }
-
-    IEnumerator DisableAfter2()
-    {
-        yield return new WaitForSeconds(2);
-
-        animator.enabled = false;
-        animator.GetComponentInParent<SpriteRenderer>().enabled = false;
-    }
-
+    
     IEnumerator ChangeSceneAfter5()
     {
-        Controller.LogStringWithReturn("that blow will be fatal. the bear will fall to you.");
+        Controller.LogStringWithReturn("that strike will be fatal. the bear will fall to you.");
         yield return new WaitForSeconds(5);
+        // todo - bear cry out sound
+        
+        
         // Controller.checkpointManager.SetCheckpoint(11); this is being set statically below
         StaticDataHolder.instance.Checkpoint = 11;
         Controller.levelLoader.LoadScene("Second Day Afternoon");
